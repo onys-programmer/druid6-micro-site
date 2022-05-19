811eaa23e3c3d5fb60190710b608841f8962e2b1
onys <thkim2017@gmail.com>
2022-05-19 14:50:45 +0900

    feat: 🎸 블로그 링크 추가

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,10 @@
 export default function BlogPage() {
   return (
     <div className="BlogPage">
-      <h1>블로그</h1>
+      <h1>
+        블로그
+      </h1>
+      <a href="https://three-catcher-318.notion.site/S204-bcec1474193d4686b3cf3324179a8642">Go To 블로그</a>
     </div>
   );
 }
