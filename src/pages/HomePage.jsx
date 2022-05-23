54025fd9e4a231ff7e8b3b66ed321c074219dcf4
박윤지 <shaian2@naver.com>
2022-05-24 06:57:13 +0900

    style: 홈 페이지 제품소개 칸에 서버 모니터링, 네트워크 모니터링 소개 추가

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import DownloadBtn from "./DownloadBtn";
 import "./HomePage.scss";
 
+import networkImage from "../static/network.png";
+
 export default function HomePage() {
   return (
     <div className="HomePage">
@@ -15,6 +17,25 @@ export default function HomePage() {
           </div>
         </div>
       </header>
+      <div className="content">
+        <div className="content-container">
+          <div className="server-monitoring">
+            <img src={networkImage} alt="network" className="images" />
+            <div className="server-monitoring-info">
+              <p className="feature-name">서버 모니터링</p>
+              <p className="feature-title">한 눈에 들어오는 실시간 서버 상태</p>
+              <p className="feature-content">
+                CPU, RAM, Disk 사용량과 실시간 트래픽 및 일별 트래픽을<br></br>
+                그래프로 한 눈에 확인해 보세요.
+              </p>
+            </div>
+          </div>
+          <hr />
+          {/* <div className="web-performance-monitoring">
+            CPU, RAM, Disk 사용량을 한 눈에 확인하세요.
+          </div> */}
+        </div>
+      </div>
     </div>
   );
 }
